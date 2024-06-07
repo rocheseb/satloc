f8667d2d6bc620b14fd8d1e5d2052fcfac7bc7f4
Sebastien Roche <sebastien.roche@mail.utoronto.ca>
2024-06-06 20:38:11 -0400

    initial commit

---
+++ satloc/satloc.py
@@ -0,0 +1,152 @@
+import argparse
+from skyfield.api import load, wgs84
+from skyfield.sgp4lib import EarthSatellite
+from skyfield.timelib import Timescale
+from datetime import datetime, UTC, timedelta
+import geoviews as gv
+import holoviews as hv
+
+hv.extension("bokeh")
+
+
+def satellite_coordinates(
+    satellite: EarthSatellite, timescale: Timescale, when: datetime = datetime.now(UTC)
+) -> tuple[float, float]:
+
+    t = timescale.utc(*when.timetuple()[:6])
+
+    latitude, longitude = [i.degrees for i in wgs84.latlon_of(satellite.at(t))]
+
+    return latitude, longitude
+
+
+def satellite_track(norad_catalog_number: int | str, when: datetime = datetime.now(UTC)):
+    celestrak_url = f"https://celestrak.org/NORAD/elements/gp.php?CATNR={norad_catalog_number}"
+    satellite = load.tle_file(celestrak_url, reload=True)[0]
+    timescale = load.timescale()
+
+    times = [when + timedelta(seconds=30 * i) for i in range(180)]
+
+    latitudes, longitudes = zip(
+        *[satellite_coordinates(satellite, timescale, when=i) for i in times]
+    )
+
+    return times, latitudes, longitudes
+
+
+def split_orbit_track(longitudes, latitudes):
+    """
+    Splits the satellite orbit path into segments to handle crossings at the 180/-180 longitude.
+    Returns a list of dictionaries where each dictionary represents a segment with "Longitude" and "Latitude" keys.
+    """
+    segments = []
+    current_segment = {"Longitude": [], "Latitude": []}
+
+    for i in range(len(longitudes)):
+        if (
+            current_segment["Longitude"]
+            and abs(longitudes[i] - current_segment["Longitude"][-1]) > 180
+        ):
+            # Split the segment if there is a crossing
+            segments.append(current_segment)
+            current_segment = {"Longitude": [], "Latitude": []}
+
+        current_segment["Longitude"].append(longitudes[i])
+        current_segment["Latitude"].append(latitudes[i])
+
+    # Add the last segment
+    if current_segment["Longitude"]:
+        segments.append(current_segment)
+
+    return segments
+
+
+def make_map(times, latitudes, longitudes, title: str = ""):
+
+    d = {"Longitude": longitudes, "Latitude": latitudes}
+
+    vdims = ["Longitude", "Latitude"]
+
+    initial_position = gv.Points([(longitudes[0], latitudes[0])], vdims=vdims)
+
+    markers_30sec = gv.Points(d, vdims=vdims)
+
+    markers_10min = gv.Points(
+        {
+            "Longitude": [v for i, v in enumerate(longitudes) if i % 20 == 0],
+            "Latitude": [v for i, v in enumerate(latitudes) if i % 20 == 0],
+        },
+        vdims=vdims,
+    )
+
+    orbit_track_segments = split_orbit_track(longitudes, latitudes)
+
+    orbit_tracks = [gv.Path(i, vdims=vdims) for i in orbit_track_segments]
+
+    map_plot = gv.tile_sources.EsriWorldStreetMap.opts(
+        width=1000, height=800, active_tools=["pan", "wheel_zoom"], title=title
+    )
+
+    for i in orbit_tracks:
+        map_plot *= i.opts(color="blue", line_width=2)
+
+    map_plot *= (
+        markers_30sec.opts(color="green", size=4)
+        * markers_10min.opts(color="orange", size=5)
+        * initial_position.opts(color="red", size=8)
+    )
+
+    div_text = f"""
+    Red marker coordinates at {datetime.strftime(times[0],'%Y-%m-%d %H:%M:%S')}<br>
+    Latitude: {latitudes[0]:.4f} °N<br>
+    Longitude: {longitudes[0]:.4f} °E<br>
+    <br>
+    Green markers predict the satellite position in 30 second intervals for 1.5 hours<br>
+    Orange markers are in 10 min intervals.
+    """
+
+    div = hv.Div(div_text).opts(width=200, height=300)
+
+    map_plot = div + map_plot
+
+    return map_plot
+
+
+def plot_satellite_tracks(
+    norad_catalog_number: int | str,
+    out_path: str = "satellite_track.",
+    when=datetime.now(UTC),
+    title: str = "",
+) -> None:
+    times, latitudes, longitudes = satellite_track(norad_catalog_number, when=when)
+
+    map_plot = make_map(times, latitudes, longitudes, title=title)
+
+    hv.save(map_plot, out_path)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("norad_catalog_number", type=int, help="NORAD catalog number")
+    parser.add_argument(
+        "-d",
+        "--date",
+        default=None,
+        help="time at which the satellite position will computed in YYYYMMDDTHHMMSS format, defaults to now",
+    )
+    parser.add_argument(
+        "-o", "--out-path", default="satellite_track.html", help="full path to the output html plot"
+    )
+    parser.add_argument("-t", "--title", default="", help="plot title")
+    args = parser.parse_args()
+
+    if args.date is None:
+        args.date = datetime.now(UTC)
+    else:
+        args.date = datetime.strptime(args.date, "%Y%M%DT%H%M%S")
+
+    plot_satellite_tracks(args.norad_catalog_number, args.out_path, args.date, args.title)
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,58 @@
+import os
+import re
+import subprocess
+from setuptools import setup, find_packages
+
+
+_mydir = os.path.dirname(__file__)
+
+# parse version number from satloc/__init__.py
+version_re = r"^__version__ = ['\"]([^'\"]*)['\"]"
+with open(os.path.join(_mydir, "satloc", "__init__.py")) as f:
+    content = f.read()
+re_search = re.search(version_re, content, re.M)
+if re_search:
+    version_str = re_search.group(1)
+else:
+    raise RuntimeError("Could not parse version string from __init__.py")
+
+# add the git hash to version_str
+if os.path.exists(os.path.join(_mydir, ".git")):
+    git_hash = subprocess.check_output(
+        "git rev-parse --verify --short HEAD", cwd=_mydir, text=True, shell=True
+    ).strip()
+    git_version_str = f"v{version_str}"
+    tags = subprocess.check_output("git tag", cwd=_mydir, text=True, shell=True)
+    if git_version_str not in tags:
+        subprocess.check_output(
+            f"git tag -a {git_version_str} {git_hash} -m 'tagged by setup.py to {version_str}'",
+            cwd=_mydir,
+            text=True,
+            shell=True,
+        )
+    version_str = f"{version_str}+git.{git_hash}"
+
+setup(
+    name="satloc",
+    description="Code to plot the orbit track of a satellite given its NORAD Catalog Number",
+    author="Sebastien Roche",
+    author_email="sroche@g.harvard.edu",
+    version=version_str,
+    url="https://github.com/rocheseb/satloc",
+    classifiers=[
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.12",
+    ],
+    install_requires=[
+        "holoviews",
+        "cartopy",
+        "geoviews",
+        "dask",
+        "skyfield",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    license="MIT",
+    python_requires=">=3.12",
+)
